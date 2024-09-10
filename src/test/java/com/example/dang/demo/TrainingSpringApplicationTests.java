a9141ef67e2d9b2be3eee19b4c292d520f8db40e
Khanhdang2811 <vkhanhdang2@gmail.com>
2024-09-10 10:43:01 +0700

    first init

---
+++ src/test/java/com/example/dang/demo/TrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
